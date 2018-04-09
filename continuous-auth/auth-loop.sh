8fda32744f02f055a20a595163ec6d1ad3fdd1aa
Emma Dauterman <evd2014@gmail.com>
2018-04-08 19:31:08 -0700

    moved setup and cleanup to proxy command

--- continuous-auth/auth-loop.sh
+++ continuous-auth/auth-loop.sh
@@ -1,7 +1,7 @@
 auth_key_cert="$HOME/.ssh/auth-key-cert.pub"
-port=2001
 token_lib="/usr/local/lib/libykcs11_NOTALINK.dylib"
-UNIX_DOMAIN_SOCKET="/tmp/socket"
+PRINCIPAL=$1
+HOST=$2
 
 function ssh-kill {
 	sleep 10s
@@ -12,10 +12,28 @@ function ssh-kill {
 	done
 }
 
-ssh-kill "ssh -q -i $auth_key_cert -o BatchMode=yes -o ConnectTimeout=5s $1@$2 nc -U $UNIX_DOMAIN_SOCKET" &
+function mux {
+    SOCKET="/tmp/socket"
+    PIPE=$SOCKET-pipe
+    mkfifo $PIPE &> /dev/null
+    rm $SOCKET &> /dev/null     # take this out when dynamically choose
+    nohup nc -lU $SOCKET -k < $PIPE | nc localhost 22 > $PIPE & 
+    echo $SOCKET 
+}
+
+function cleanup {
+    SOCKET=$1
+    PIPE=$1-pipe
+    kill $(ps aux | grep "nc -lU $1 -k < $PIPE" | grep -v grep | awk '{print $2}') &> /dev/null
+    kill $(ps aux | grep "nc localhost 22 > $PIPE" | grep -v grep | awk '{print $2}') &> /dev/null
+    rm $SOCKET
+}
+
+SOCKET="$(ssh -q -i $auth_key_cert -o BatchMode=yes $PRINCIPAL@$HOST "$(typeset -f); mux" < /dev/null)"
+ssh-kill "ssh -q -i $auth_key_cert -o BatchMode=yes -o ConnectTimeout=5s $1@$2 nc -U $SOCKET" &
 while kill -0 $PPID > /dev/null
 do
-	if ! ssh -q -i $auth_key_cert -o BatchMode=yes -o ConnectTimeout=5s $1@$2 nc -U $UNIX_DOMAIN_SOCKET
+	if ! ssh -q -i $auth_key_cert -o BatchMode=yes -o ConnectTimeout=5s $1@$2 nc -U $SOCKET
     then
         sleep 1s   # wait to retry to see if key inserted
         if (lsusb | grep -q "Yubikey") &> /dev/null
@@ -25,4 +43,5 @@ do
         fi
     fi
 done
+ssh -q -i $auth_key_cert -o BatchMode=yes $PRINCIPAL@$HOST "$(typeset -f); cleanup $SOCKET"
 
--- continuous-auth/ssh-continuous-auth.sh
+++ continuous-auth/ssh-continuous-auth.sh
@@ -8,25 +8,8 @@ auth_pub_key="$HOME/.ssh/auth-key.pub"
 principal="$(cut -d'@' -f1 <<<$1)"
 host="$(cut -d'@' -f2 <<<$1)"
 
-function mux {
-    port=2001                   # Port to forward to
-    UNIX_DOMAIN_SOCKET="/tmp/socket"
-    pipe=$UNIX_DOMAIN_SOCKET-pipe
-    mkfifo $pipe &> /dev/null
-    rm $UNIX_DOMAIN_SOCKET &> /dev/null     # take this out when dynamically choose
-    nohup nc -lU $UNIX_DOMAIN_SOCKET -k < $pipe | nc localhost 22 > $pipe &
-}
-
-function cleanup {
-    kill $(ps aux | grep "nc -lU $1 -k < $1-pipe" | grep -v grep | awk '{print $2}') &> /dev/null
-    kill $(ps aux | grep "nc localhost 22 > $1-pipe" | grep -v grep | awk '{print $2}') &> /dev/null
-    # do netstat for unix domain sockets, search for it and find process and kill it, and delete unix domain socket 
-}
-
 ssh-keygen -D $token_lib -s $ca_pub_key -I $USER -n $principal $auth_pub_key &> /dev/null # used to be -I user 
 
-ssh -q -i $auth_key_cert -o BatchMode=yes $principal@$host "$(typeset -f); mux"
-ssh -o 'ProxyCommand bash -c "source auth-loop.sh %r %h"' -o ConnectTimeout=5s -i $auth_key_cert $1
-ssh -q -i $auth_key_cert -o BatchMode=yes $principal@$host "$(typeset -f); cleanup /tmp/socket"
+ssh -o 'ProxyCommand bash -c "source auth-loop.sh %r %h $SOCKET"' -o ConnectTimeout=5s -i $auth_key_cert $1
 
 rm $auth_key_cert
