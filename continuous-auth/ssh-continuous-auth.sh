4067fc2a98f0d83c908ac6de2073aed58c114384
Emma Dauterman <evd2014@gmail.com>
2018-04-08 22:24:05 -0700

    Support for multiple connections

--- continuous-auth/ssh-continuous-auth.sh
+++ continuous-auth/ssh-continuous-auth.sh
@@ -1,15 +1,23 @@
 # Already need to have added PKCS11 library for token and have a pub key
 
 token_lib="/usr/local/lib/libykcs11_NOTALINK.dylib"
-auth_key_cert="$HOME/.ssh/auth-key-cert.pub"
+AUTH_KEY_CERT="$HOME/.ssh/auth-key-cert.pub"
 ca_pub_key="/etc/ssh/ssh-ca-key.pub"
-auth_pub_key="$HOME/.ssh/auth-key.pub"
+AUTH_PUB_KEY="$HOME/.ssh/auth-key.pub"
 
 principal="$(cut -d'@' -f1 <<<$1)"
 host="$(cut -d'@' -f2 <<<$1)"
 
-ssh-keygen -D $token_lib -s $ca_pub_key -I $USER -n $principal $auth_pub_key &> /dev/null # used to be -I user 
+if [ ! -f $AUTH_KEY_CERT ]
+then
+    echo "Generating new certificate"
+    ssh-keygen -D $token_lib -s $ca_pub_key -I $USER -n $principal $AUTH_PUB_KEY &> /dev/null # used to be -I user 
+fi
 
-ssh -o 'ProxyCommand bash -c "source auth-loop.sh %r %h $SOCKET"' -o ConnectTimeout=5s -i $auth_key_cert $1
+ssh -o 'ProxyCommand bash -c "source auth-loop.sh %r %h $AUTH_PUB_KEY"' -o ConnectTimeout=5s -i $AUTH_KEY_CERT $1
 
-rm $auth_key_cert
+if ! kill -0 $(ps aux | grep "ssh .* -i $AUTH_KEY_CERT" | grep -v grep | awk '{print $2}') &> /dev/null
+then
+    echo "No other ssh process"
+    rm $AUTH_KEY_CERT
+fi
