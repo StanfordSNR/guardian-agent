dd8e1990b3159fc15c1cb0c2e298fb3d3a528fb5
Emma Dauterman <evd2014@gmail.com>
2018-05-16 20:53:33 -0700

    kill from remote side, just end connection when take out yubikey

--- continuous-auth/auth-loop.sh
+++ continuous-auth/auth-loop.sh
@@ -1,22 +1,20 @@
 AUTH_KEY_CERT="$HOME/.ssh/auth-key-cert.pub"
-TOKEN_LIB="/usr/local/lib/libykcs11_NOTALINK.dylib"
+TOKEN_LIB="/usr/local/lib/libykcs11_YUBICO.dylib"
 PRINCIPAL=$1
 HOST=$2
 
 function ssh-kill {
 	sleep 10s
-	while kill -0 $PPID > /dev/null &> /dev/null
-	do
-        kill $(ps aux | grep "$1" | grep -v grep | awk '{print $2}') &> /dev/null
-        sleep 10s
-	done
+    conn=$(ps aux | grep "$1" | grep -v grep | awk '{print $2}') 
+    kill $conn &> /dev/null 
+    wait $conn &> /dev/null
 }
 
 function mux {
     SOCKET="$(mktemp /tmp/cont-auth-socket.XXXXXX)"
     PIPE=$SOCKET-pipe
     mkfifo $PIPE &> /dev/null
-    rm $SOCKET &> /dev/null     # take this out when dynamically choose
+    rm $SOCKET &> /dev/null
     nohup nc -lU $SOCKET -k < $PIPE | nc localhost 22 > $PIPE & 
     echo "$SOCKET" 
 }
@@ -31,17 +29,12 @@ function cleanup {
 }
 
 SOCKET="$(ssh -q -i $AUTH_KEY_CERT -o BatchMode=yes $PRINCIPAL@$HOST "$(typeset -f); mux" < /dev/null)"
-ssh-kill "ssh -q -i $AUTH_KEY_CERT -o BatchMode=yes -o ConnectTimeout=5s $1@$2 nc -U $SOCKET" &
 while kill -0 $PPID > /dev/null
 do
-	if ! ssh -q -i $AUTH_KEY_CERT -o BatchMode=yes -o ConnectTimeout=5s $1@$2 nc -U $SOCKET
-    then
-        sleep 1s   # wait to retry to see if key inserted
-        if (lsusb | grep -q "Yubikey") &> /dev/null
-        then
-            eval `ssh-agent -s` > /dev/null
-            ssh-add -s $TOKEN_LIB > /dev/null
-        fi
+    ssh -q -i $AUTH_KEY_CERT -o BatchMode=yes -o ConnectTimeout=5s $1@$2 "$(typeset -f); ssh-kill 'nc -U $SOCKET'" &
+    ssh -q -i $AUTH_KEY_CERT -o BatchMode=yes -o ConnectTimeout=5s $1@$2 "nc -U $SOCKET"
+    if ! (lsusb | grep -q "Yubikey") &> /dev/null; then
+        break
     fi
 done
 ssh -q -i $AUTH_KEY_CERT -o BatchMode=yes $PRINCIPAL@$HOST "$(typeset -f); cleanup $SOCKET $PIPE"
--- continuous-auth/ssh-continuous-auth.sh
+++ continuous-auth/ssh-continuous-auth.sh
@@ -1,19 +1,31 @@
 # Already need to have added PKCS11 library for token and have a pub key
 
-TOKEN_LIB="/usr/local/lib/libykcs11_NOTALINK.dylib"
+TOKEN_LIB="/usr/local/lib/libykcs11_YUBICO.dylib"
 AUTH_KEY_CERT="$HOME/.ssh/auth-key-cert.pub"
 CA_PUB_KEY="/etc/ssh/ssh-ca-key.pub"
 AUTH_PUB_KEY="$HOME/.ssh/auth-key.pub"
 
 principal="$(cut -d'@' -f1 <<<$1)"
 host="$(cut -d'@' -f2 <<<$1)"
+stop=false
+
+function read_into_pipe {
+    while read line < /dev/stdin; do echo $line > $PIPE_IN; done
+}
 
 if [ ! -f $AUTH_KEY_CERT ]
 then
     ssh-keygen -D $TOKEN_LIB -s $CA_PUB_KEY -I $USER -n $principal $AUTH_PUB_KEY &> /dev/null # used to be -I user 
 fi
 
-ssh -o 'ProxyCommand bash -c "source auth-loop.sh %r %h $AUTH_PUB_KEY"' -o ConnectTimeout=5s -i $AUTH_KEY_CERT $1
+PIPE_IN="pipe-in"
+mkfifo $PIPE_IN
+PIPE_OUT="pipe-out"
+mkfifo $PIPE_OUT
+
+#read_into_pipe &
+
+ssh -o 'ProxyCommand bash -c "source auth-loop.sh %r %h $AUTH_PUB_KEY $PID"' -o ConnectTimeout=5s -i $AUTH_KEY_CERT -tt $1
 
 if ! kill -0 $(ps aux | grep "ssh .* -i $AUTH_KEY_CERT" | grep -v grep | awk '{print $2}') &> /dev/null
 then
